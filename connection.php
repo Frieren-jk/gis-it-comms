6445616b0c4204a413415968d64f0fa09f7a8d45
JK <h20salvador@gmail.com>
2025-08-01 11:30:52 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,6 @@
 <?php
-$host = "localhost";
-$dbname = "gis-comms";
-$username = "root";
-$password = ""; 
+ //   $host = "localhost"; $dbname = "gis-comms"; $username = "root"; $password = ""; 
+    $host = "10.15.31.19"; $dbname = "gis-comms"; $username = "remote_user"; $password = "password1234";
 
 $conn = new mysqli($host, $username, $password, $dbname);
 

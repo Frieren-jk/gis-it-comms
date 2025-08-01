3a7b661d0d3fa6ea3faab95c0475c55465d33d46
JK <h20salvador@gmail.com>
2025-08-01 16:09:00 +0800

    Update logout.php

--- actions/logout.php
+++ actions/logout.php
@@ -1,13 +1,27 @@
 <?php
 session_start();
 
-// Destroy all session variables
+// Unset all session variables
 $_SESSION = array();
 
-// Destroy the session itself
+// If there's a session cookie, delete it
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Destroy the session
 session_destroy();
 
-// Redirect to login page (change path as needed)
+// Redirect to login page
 header("Location: ../login.php");
 exit();
 ?>
